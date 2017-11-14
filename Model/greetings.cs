2e3226a5f6d7af2ca06bb6fe5bb51c25c8a51b95
Anna <annadonlin@gmail.com>
2017-11-14 07:33:37 -0800

    cleaned up _Form.cshtml and greetings.cs

--- Model/greetings.cs
+++ Model/greetings.cs
@@ -22,34 +22,39 @@ namespace halloween.Model
         public string sendTo { get; set; }
 
         [DisplayName("To Name")]
+        [Display(Prompt = "Your friend's name")]
         [Required(ErrorMessage = "Required")]
+        [StringLength(70, MinimumLength = 3, ErrorMessage = "You must enter between 3 to 70 characters")]
         public string toName { get; set; }
 
         [DisplayName("Email")]
+        [Display(Prompt = "Your friend's email")]
         [Required(ErrorMessage = "Required")]
         public string toEmail { get; set; }
 
         [DisplayName("Title")]
-        [Display(Prompt = "Enter scary title")]
+        [Display(Prompt = "Customize your title here.")]
         [Required(ErrorMessage = "Required")]
         public string title { get; set; }
 
         [DisplayName("Message")]
-        [Display(Prompt = "Write your scary message")]
+        [Display(Prompt = "Personalize your message here.")]
         [Required(ErrorMessage = "Required")]
         [StringLength(150, MinimumLength = 3, ErrorMessage = "Enter between 3 and 150 characters.")]
         public string message { get; set; }
 
         [DisplayName("Sent From")]
         public string sentFrom { get; set; }
-        [Required(ErrorMessage = "Required")]
-        [DisplayName("From Name")]
 
+        [DisplayName("From Name")]
+        [Display(Prompt = "Your name")]
         public string fromName { get; set; }
         [Required(ErrorMessage = "Required")]
-        [DisplayName("Email")]
 
+        [DisplayName("Email")]
+        [Display(Prompt = "Your email")]
         public string fromEmail { get; set; }
+        [Required(ErrorMessage = "Required")]
  
         [DisplayName("I agree")]
         public string agree { get; set; }
